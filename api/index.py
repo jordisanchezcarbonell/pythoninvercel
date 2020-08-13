692928cefc0896351f9ae09d4456177c87c21a9e
Jordi <kazukunpayeer@gmail.com>
2020-08-13 21:48:00 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,14 +4,7 @@ from datetime import datetime
 from cowpy import cow
 
 
-class Contador(object):
-
-    def __init__(self, inicial=0):
-        self.numero = inicial
-
-    def siguiente(self):
-        self.numero += 1
-        return self.numero
+NUMBER = 5
 
 
 class handler(BaseHTTPRequestHandler):
@@ -22,15 +15,8 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         message = cow.Cowacter().milk('Hello from Python from a Serverless Function! Now Time is it: ' +
-                                      str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+                                      str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+str(NUMBER))
         self.wfile.write(
             bytes("<html><head><title>Title goes here.</title></head>/html>", "utf-8"))
 
-        self.wfile.write(message.encode())
-
-        cuenta = Contador()
-
-        for i in range(5):
-            self.wfile.write(str(cuenta.siguiente()))
-            cuenta.siguiente()
         return
