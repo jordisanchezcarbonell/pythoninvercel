315b384414da1bede83fa582363af5552bf02ccc
Jordi <kazukunpayeer@gmail.com>
2020-08-13 22:00:36 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,29 +4,22 @@ from datetime import datetime
 from cowpy import cow
 
 
-class handler(BaseHTTPRequestHandler):
-
-
 NUMBER = 5
 
 
-def do_GET(self):
+class handler(BaseHTTPRequestHandler):
 
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    message = cow.Cowacter().milk('Hello from Python from a Serverless Function! Now Time is it: ' +
-                                  str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+' ESTO FUNCIONA : '+str(NUMBER))
+    def do_GET(self):
 
-    self.wfile.write(
-        bytes("<html><head><title>Title goes here.</title></head>/html>", "utf-8"))
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        message = cow.Cowacter().milk('Hello from Python from a Serverless Function! Now Time is it: ' +
+                                      str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+' ESTO FUNCIONA : '+str(NUMBER))
 
-    self.wfile.write(message.encode())
+        self.wfile.write(
+            bytes("<html><head><title>Title goes here.</title></head>/html>", "utf-8"))
 
-    NUMBER += 1
+        self.wfile.write(message.encode())
 
-    TEST = cow.Cowacter().milk('Hello from Python from a Serverless Function! Now Time is it: ' +
-                               str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+' ESTO FUNCIONA : '+str(NUMBER))
-
-    self.wfile.write(TEST.encode())
-    return
+        return
