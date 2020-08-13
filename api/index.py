aabad31834a0dda7c15a514ea901ad76a0d3778c
Jordi <kazukunpayeer@gmail.com>
2020-08-13 21:50:11 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,13 +10,16 @@ NUMBER = 5
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        cuenta = Contador()
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         message = cow.Cowacter().milk('Hello from Python from a Serverless Function! Now Time is it: ' +
                                       str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+str(NUMBER))
+
         self.wfile.write(
             bytes("<html><head><title>Title goes here.</title></head>/html>", "utf-8"))
 
+        self.wfile.write(message.encode())
         return
