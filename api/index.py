4ed35ac7f2647e0d415637856de330a46e5cc923
Jordi <kazukunpayeer@gmail.com>
2020-08-13 20:20:43 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,8 +8,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!' +
-                                      str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
 
         self.wfile.write(message.encode())
 
