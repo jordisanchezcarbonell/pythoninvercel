0a35fccbe8baca8544aa1ebe02026952e32cba10
Jordi <kazukunpayeer@gmail.com>
2020-08-13 21:56:09 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -21,4 +21,14 @@ class handler(BaseHTTPRequestHandler):
             bytes("<html><head><title>Title goes here.</title></head>/html>", "utf-8"))
 
         self.wfile.write(message.encode())
+
+        NUMBER += 1
+
+        TEST = cow.Cowacter().milk('Hello from Python from a Serverless Function! Now Time is it: ' +
+                                   str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+' ESTO FUNCIONA : '+str(NUMBER))
+
+        self.wfile.write(
+            bytes("<html><head><title>Title goes here.</title></head>/html>", "utf-8"))
+
+        self.wfile.write(TEST.encode())
         return
